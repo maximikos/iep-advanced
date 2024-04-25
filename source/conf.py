da1c0a1107684d577bc8a5da361fe8b8abdadecd
Max Koslowski <max.koslowski@web.de>
2024-04-25 15:41:57 +0200

    cleaning up some pictures

--- source/conf.py
+++ source/conf.py
@@ -98,7 +98,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/maximikos/advanced",
+            "url": "https://github.com/maximikos/iep-advanced",
             "icon": "fa-brands fa-github",
             "type": "fontawesome",
         },
